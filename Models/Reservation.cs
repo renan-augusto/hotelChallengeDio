b11e06f68f3ec8077c4dbdc92a555c946557559b
renan-augusto <renan_acm1992@hotmail.com>
2023-03-19 17:38:31 -0300

    Encaminhando solucao do desafio DIO

--- Models/Reservation.cs
+++ Models/Reservation.cs
@@ -9,18 +9,22 @@ namespace hotelChallengeDio.Models
         public Suite Suite { set; get; }
         public int DaysReservation { set; get; }
 
+        public Reservation() { }
+
+        public Reservation(int daysReservation)
+        {
+            DaysReservation = daysReservation;
+        }
+
         public void InputGuests(List<Person> guests)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            if (Suite.Capacity >= guests.Count)
             {
                 Guests = guests;
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                throw new Exception("Numero de hospedes excede a capacidade da suite");
             }
         }
 
@@ -28,25 +32,25 @@ namespace hotelChallengeDio.Models
         {
             Suite = suite;    
         }
-        public void DisplayGuestsQtd()
+        public int DisplayGuestsQtd()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            return Guests.Count;   
         }
         public decimal PriceCalculator()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;  
-             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            decimal finalPrice = (decimal)DaysReservation * Suite.Price;  
+            
+          
+            if (DaysReservation >= 10)
             {
-                valor = 0;
+                finalPrice = finalPrice * (decimal)0.90;
+                return finalPrice;
             } 
-            return valor;     
+            else 
+            {
+                 return finalPrice;  
+            }
+              
         }
 
     }
--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
 ﻿using System.Text;
-using hotelChallengeDio;
+using hotelChallengeDio.Models;
 
 Console.OutputEncoding = Encoding.UTF8;
 
-List<Pessoa> guests = new List<Pessoa>();
+List<Person> guests = new List<Person>();
 
 Person p1 = new Person(name: "Renan Augusto");
 Person p2 = new Person(name: "Fernanda Lavezzo");
@@ -11,9 +11,9 @@ Person p2 = new Person(name: "Fernanda Lavezzo");
 guests.Add(p1);
 guests.Add(p2);
 
-Suite suite = new Suite(SuiteType: "Convencional", Capacity: 3, price: 90);
+Suite suite = new Suite(suiteType: "Convencional", capacity: 3, price: 90);
 
-Reservation reservation = new Reservation(DaysReservation: 5);
+Reservation reservation = new Reservation(daysReservation: 5);
 reservation.InputSuite(suite);
 reservation.InputGuests(guests);
 
